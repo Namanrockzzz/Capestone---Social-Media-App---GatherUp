f8110c978b450cc1c30bbf81cc2b5a602c129791
Namanrockzzz <naman.taneja123@gmail.com>
2020-11-20 15:55:08 +0530

    change in settings.py

--- GatherUP/settings.py
+++ GatherUP/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(j6p2!t%xqc-ecq#))bg$h91wpzq_+caa&rn5$*)o0$yl9_5#l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
